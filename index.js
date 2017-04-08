2ac94e95ce0e22122e6ceba06051c5b4a3b180cf
Phillip Gibson <gibson.phillip.j@gmail.com>
2017-04-08 19:55:53 -0400

    Minor code optimizations.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const exec = require('simple-exec-promise')
 
-const makeProxy = (o, stack = []) => new Proxy(o, {
+const makeProxy = (o = {}, stack = []) => new Proxy(o, {
   get (_, k) {
     return makeProxy((...args) => {
-      return exec(`${[].concat(stack, k, args).join(' ')}`)
+      return exec([].concat(stack, k, args).join(' '))
     }, [].concat(stack, k))
   }
 })
 
-module.exports = makeProxy({})
+module.exports = makeProxy()
